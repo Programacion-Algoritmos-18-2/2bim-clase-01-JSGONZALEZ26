b756e935afb584ba1e5022f52ec299f523170a84
Jairo Gonzalez <jairo99gc@icloud.com>
2018-11-30 11:32:59 -0500

    Subiendo archivos

---
+++ ejercicio-clases/manejo-excepciones/principa2.py
@@ -0,0 +1,15 @@
+try:	
+	numero1 = input("Ingrese un número: \n")
+	numero1 = int(numero1)
+	numero2 = input("Ingrese un número: \n")
+	numero2 = int(numero2)
+	operacion = numero1 / numero2
+	print("El valor de la operación es %d" % (operacion))
+except NameError as e:
+	print("Existe un error %s" % e)
+except ValueError as e:
+	print("Existe un error %s" % e)
+except ZeroDivisionError as e:
+	print("Existe un error (%s) : %s"  % (e.__class__, e))
+#except Exception as e:
+#	print("Existe un error (%s) : %s"  % (e.__class__, e))
